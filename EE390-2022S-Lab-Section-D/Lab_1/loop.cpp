e5705ee66e010be2367d6a2a1a24c3548084e0da
Angel Tomas Ordonez Retamar <98297514+angelordonezretamar@users.noreply.github.com>
2023-03-29 12:05:17 -0400

    Create loop.cpp

---
+++ EE390-2022S-Lab-Section-D/Lab_1/loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int start, end;
+    cout << "Enter your first number, then your second number." << "\n";
+    cin >> start >> end;
+    for(start; start <= end; start++) {
+
+    cout << start;
+
+    }
+        
+    return 0;
+}
